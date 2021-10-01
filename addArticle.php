69049ba9c1aaf1708c751237b07e7b315e14f99a
Aryan Sethi <aryan.rdps@gmail.com>
2021-10-01 14:25:09 +0530

    added functionality to add article from admin

--- addArticle.php
+++ addArticle.php
@@ -5,62 +5,50 @@
 <div class="container ps-5  py-5 login-container">
 
     <h1 class="text-center"> Admin Panel </h1>
-    <h3> Adding a blog </h3>
+    <h3> Adding an article </h3>
 
     <div class="course-form">
-        <form enctype="multipart/form-data" method="POST" action="addBlog.php">
+        <form enctype="multipart/form-data" method="POST" action="addArticle.php">
 
             <!-- General intro -->
             <div class="course-box course-title">
-                <h3 class="text-center"> Blog-General section </h3>
+                <h3 class="text-center"> Article-General section </h3>
                 <div class="mb-4">
-                    <label for="blog-title" class="form-label"> Blog Title </label>
-                    <input name="blog-title" placeholder="Give a title to Blog" type="text" class="form-control" id="blog-title">
+                    <label for="article-heading" class="form-label"> Article Heading </label>
+                    <input name="article-heading" placeholder="Give a heading to Article" type="text" class="form-control" id="article-heading">
                 </div>
 
                 <div class="mb-4">
-                    <label for="blog-heading" class="form-label"> Blog Heading </label>
-                    <input name="blog-heading" placeholder="Give a meaningful heading" type="text" class="form-control" id="blog-heading">
+                    <label for="article-subheading" class="form-label"> Article Sub-Heading </label>
+                    <input name="article-subheading" placeholder="Give a meaningful subheading" type="text" class="form-control" id="article-subheading">
                 </div>
 
                 <div class="mb-4">
-                    <label for="blog-date-posted" class="form-label"> Blog date posted </label>
-                    <input name="blog-date-posted" placeholder="Enter date posted" type="text" class="form-control" id="blog-date-posted">
+                    <label for="article-date-posted" class="form-label"> Article date posted </label>
+                    <input name="article-date-posted" placeholder="Enter date posted" type="date" class="form-control" id="article-date-posted">
                 </div>
 
                 <div class="mb-4">
-                    <label for="blog-image" class="form-label"> Blog Image </label>
-                    <input name="blog-image"  type="file" accept="image/*" class="form-control" id="blog-image">
+                    <label for="article-image" class="form-label"> Article Image </label>
+                    <input name="article-image"  type="file" accept="image/*" class="form-control" id="article-image">
                 </div>
 
                 <div class="mb-4">
-                    <label for="blog-content" class="form-label"> Content </label>
-                    <textarea name="blog-content" placeholder="Enter content for the Blog" type="text" class="form-control" id="blog-content"></textarea>
+                    <label for="article-content" class="form-label"> Content </label>
+                    <textarea name="article-content" placeholder="Enter content for the Article" type="text" class="form-control" id="article-content"></textarea>
                 </div>
                 <script>
                     ClassicEditor
-                        .create( document.querySelector( '#blog-content' ) )
+                        .create( document.querySelector( '#article-content' ) )
                         .catch( error => {
                             console.error( error );
                         } );
                 </script>
             </div>
 
-            <!-- Author Section -->
-            <div class="course-box">
-                <h3 class="text-center"> Blog-Author section </h3>
-                <div class="mb-4">
-                    <label for="blog-author" class="form-label"> Author Name </label>
-                    <input name="blog-author" placeholder="Enter name of author" type="text" class="form-control" id="blog-author">
-                </div>
-                <div class="mb-4">
-                    <label for="blog-author-image" class="form-label"> Author Image </label>
-                    <input name="blog-author-image" class="form-control" type="file" id="blog-author-image" accept="image/*">
-                </div>
-            </div>
 
             <div class="d-flex justify-content-center gap-4">
-                    <input name="submit" type="submit" value="Add to Blogs" class="btn btn-lg btn-primary">
+                    <input name="submit" type="submit" value="Add to Articles" class="btn btn-lg btn-primary">
             </div>
         </form>
     </div>
@@ -73,36 +61,36 @@
         // db
         require_once "php/db.inc.php";
 
-        $title = $_POST["blog-title"];
-        $heading = $_POST["blog-heading"];
-        $datePosted = $_POST["blog-date-posted"];
-        $content = addslashes($_POST["blog-content"]);
-        $author = $_POST["blog-author"];
+        $heading = $_POST["article-heading"];
+        $subheading = $_POST["article-subheading"];
+        $datePosted = $_POST["article-date-posted"];
+        $content = addslashes($_POST["article-content"]);
 
-        $blogImage = $_FILES["blog-image"]["name"];
-        $authorImage = $_FILES["blog-author-image"]["name"];
+        $articleImage = $_FILES["article-image"]["name"];
 
         //uploading images to blog directory
-        $uploaddir = 'img/blog/';
-        $uploadfileBlog = $uploaddir . basename($blogImage);
-        $uploadfileAuthor = $uploaddir . basename($authorImage);
+        $uploaddir = 'img/article/';
+        $uploadfileArticle = $uploaddir . basename($articleImage);
 
-        move_uploaded_file($_FILES['blog-image']['tmp_name'], $uploadfileBlog);
-        move_uploaded_file($_FILES['blog-author-image']['tmp_name'], $uploadfileAuthor);
-
-        // Connection to Database
+        move_uploaded_file($_FILES['article-image']['tmp_name'], $uploadfileArticle);
         
-        $table_name = "blog";
+        $table_name = "article";
 
-        $query = "INSERT INTO `$table_name` ( `blog_heading`, `blog_status`, `blog_author`, `blog_title`, `blog_content`, `blog_image`, `author_image`, `date_posted`) VALUES ('$heading', 'accept', '$author', '$title', '$content', '$blogImage', '$authorImage', '$datePosted'); ";
+
+        if (empty($datePosted)){
+            $query = "INSERT INTO `$table_name` ( `article_heading`, `article_status`,  `article_subheading`, `article_content`, `article_image` , `date_posted`) VALUES ('$heading', 'accept', '$subheading', '$content' , '$articleImage',  UTC_TIMESTAMP()); ";
+        }
+
+        else{
+            $query = "INSERT INTO `$table_name` ( `article_heading`, `article_status`,  `article_subheading`, `article_content`, `article_image` , `date_posted`) VALUES ('$heading', 'accept', '$subheading', '$content' , '$articleImage',  '$datePosted'); ";
+        }
 
         $insertion = mysqli_query($conn , $query);
-        // $conn->close();
         if (!$insertion){
-            echo "<script> alert('Blog Failed $conn->error') </script>" ;
+            echo "<script> alert('Article Failed $conn->error') </script>" ;
         }
         else{
-            echo "<script> alert('Blog added Successfully ') </script>" ;
+            echo "<script> alert('Article added Successfully ') </script>" ;
         }
         
     }
